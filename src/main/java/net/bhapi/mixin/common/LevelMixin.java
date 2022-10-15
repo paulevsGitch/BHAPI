a2bf173d5b4a8b2949170d52e36ab7177586b629
paulevsGitch <paulevs@yandex.ru>
2022-10-15 12:22:58 +0300

    Small fixes

--- src/main/java/net/bhapi/mixin/common/LevelMixin.java
+++ src/main/java/net/bhapi/mixin/common/LevelMixin.java
@@ -37,54 +37,27 @@ import java.util.Random;
 import java.util.Set;
 
 @Mixin(Level.class)
-public abstract class LevelMixin implements BlockStateProvider {
+public abstract class LevelMixin {
 	@Shadow @Final protected DimensionData dimensionData;
-	
 	@Shadow private Set field_194;
-	
 	@Shadow public List players;
-	
 	@Shadow private int field_195;
-	
-	@Shadow public abstract Chunk getChunkFromCache(int i, int j);
-	
 	@Shadow protected int field_203;
-	
-	@Shadow public abstract int getLightLevel(int i, int j, int k);
-	
 	@Shadow public Random rand;
-	
-	@Shadow public abstract PlayerBase getClosestPlayer(double d, double e, double f, double g);
-	
-	@Shadow public abstract int getLight(LightType arg, int i, int j, int k);
-	
-	@Shadow public abstract void playSound(double d, double e, double f, String string, float g, float h);
-	
-	@Shadow public abstract boolean isRaining();
-	
-	@Shadow public abstract boolean isThundering();
-	
-	@Shadow public abstract int getHeightIterating(int i, int j);
-	
-	@Shadow public abstract boolean canRainAt(int i, int j, int k);
-	
-	@Shadow public abstract boolean method_184(BaseEntity arg);
-	
 	@Shadow protected int field_209;
 	
+	@Shadow public abstract Chunk getChunkFromCache(int i, int j);
+	@Shadow public abstract int getLightLevel(int i, int j, int k);
+	@Shadow public abstract PlayerBase getClosestPlayer(double d, double e, double f, double g);
+	@Shadow public abstract int getLight(LightType arg, int i, int j, int k);
+	@Shadow public abstract void playSound(double d, double e, double f, String string, float g, float h);
+	@Shadow public abstract boolean isRaining();
+	@Shadow public abstract boolean isThundering();
+	@Shadow public abstract int getHeightIterating(int i, int j);
+	@Shadow public abstract boolean canRainAt(int i, int j, int k);
+	@Shadow public abstract boolean method_184(BaseEntity arg);
 	@Shadow public abstract BiomeSource getBiomeSource();
-	
 	@Shadow public abstract boolean setBlock(int i, int j, int k, int l);
-	
-	@Override
-	public boolean setBlockState(int x, int y, int z, BlockState state) {
-		return false;
-	}
-	
-	@Override
-	public BlockState getBlockState(int x, int y, int z) {
-		return null;
-	}
 	
 	@Inject(
 		method = "<init>(Lnet/minecraft/level/dimension/DimensionData;Ljava/lang/String;Lnet/minecraft/level/dimension/BaseDimension;J)V",
@@ -108,12 +81,6 @@ public abstract class LevelMixin implements BlockStateProvider {
 	)
 	private void bhapi_onWorldInit3(DimensionData data, String name, long seed, BaseDimension dimension, CallbackInfo info) {
 		loadBlockStates();
-	}
-	
-	@Inject(method = "getMaterial(III)Lnet/minecraft/block/material/Material;", at = @At("HEAD"), cancellable = true)
-	private void getMaterial(int x, int y, int z, CallbackInfoReturnable<Material> info) {
-		BlockState state = getBlockState(x, y, z);
-		info.setReturnValue(state == null ? Material.AIR : state.getBlock().material);
 	}
 	
 	@Inject(method = "saveLevelData()V", at = @At("HEAD"))
@@ -141,10 +108,7 @@ public abstract class LevelMixin implements BlockStateProvider {
 	@Inject(method = "method_248", at = @At("HEAD"), cancellable = true)
 	private void bhapi_method_248(CallbackInfo info) {
 		info.cancel();
-		int px;
-		int j;
-		int chunkZ;
-		int chunkX;
+		int px, index, chunkZ, chunkX;
 		this.field_194.clear();
 		
 		for (int i = 0; i < this.players.size(); ++i) {
@@ -164,32 +128,30 @@ public abstract class LevelMixin implements BlockStateProvider {
 		}
 		
 		for (Object object : this.field_194) {
-			int n6;
-			int py;
-			int pz;
-			chunkX = ((Vec2i)object).x * 16;
-			chunkZ = ((Vec2i)object).z * 16;
+			int blockID, py, pz;
+			chunkX = ((Vec2i)object).x << 4;
+			chunkZ = ((Vec2i)object).z << 4;
 			Chunk chunk = this.getChunkFromCache(((Vec2i)object).x, ((Vec2i)object).z);
 			
 			if (this.field_195 == 0) {
 				PlayerBase playerBase;
 				this.field_203 = this.field_203 * 3 + 1013904223;
-				j = this.field_203 >> 2;
-				px = j & 0xF;
-				pz = j >> 8 & 0xF;
-				py = j >> 16 & 0x7F;
-				n6 = chunk.getBlockId(px, py, pz);
-				if (n6 == 0 && this.getLightLevel(px += chunkX, py, pz += chunkZ) <= this.rand.nextInt(8) && this.getLight(LightType.SKY, px, py, pz) <= 0 && (playerBase = this.getClosestPlayer(px + 0.5, py + 0.5, pz + 0.5, 8.0)) != null && playerBase.squaredDistanceTo(px + 0.5, py + 0.5, pz + 0.5) > 4.0) {
-					this.playSound((double)px + 0.5, (double)py + 0.5, (double)pz + 0.5, "ambient.cave.cave", 0.7f, 0.8f + this.rand.nextFloat() * 0.2f);
+				index = this.field_203 >> 2;
+				px = index & 15;
+				pz = index >> 8 & 15;
+				py = index >> 16 & 128;
+				blockID = chunk.getBlockId(px, py, pz);
+				if (blockID == 0 && this.getLightLevel(px += chunkX, py, pz += chunkZ) <= this.rand.nextInt(8) && this.getLight(LightType.SKY, px, py, pz) <= 0 && (playerBase = this.getClosestPlayer(px + 0.5, py + 0.5, pz + 0.5, 8.0)) != null && playerBase.squaredDistanceTo(px + 0.5, py + 0.5, pz + 0.5) > 4.0) {
+					this.playSound(px + 0.5, py + 0.5, pz + 0.5, "ambient.cave.cave", 0.7f, 0.8f + this.rand.nextFloat() * 0.2f);
 					this.field_195 = this.rand.nextInt(12000) + 6000;
 				}
 			}
 			
 			if (this.rand.nextInt(100000) == 0 && this.isRaining() && this.isThundering()) {
 				this.field_203 = this.field_203 * 3 + 1013904223;
-				j = this.field_203 >> 2;
-				px = chunkX + (j & 0xF);
-				pz = chunkZ + (j >> 8 & 0xF);
+				index = this.field_203 >> 2;
+				px = chunkX + (index & 0xF);
+				pz = chunkZ + (index >> 8 & 0xF);
 				py = this.getHeightIterating(px, pz);
 				if (this.canRainAt(px, py, pz)) {
 					this.method_184(new Lightning(Level.class.cast(this), px, py, pz));
@@ -199,31 +161,31 @@ public abstract class LevelMixin implements BlockStateProvider {
 			
 			if (this.rand.nextInt(16) == 0) {
 				this.field_203 = this.field_203 * 3 + 1013904223;
-				j = this.field_203 >> 2;
-				px = j & 0xF;
-				pz = j >> 8 & 0xF;
-				py = this.getHeightIterating(px + chunkX, pz + chunkZ);
-				if (this.getBiomeSource().getBiome(px + chunkX, pz + chunkZ).canSnow() && py >= 0 && py < 128 && chunk.getLight(LightType.BLOCK, px, py, pz) < 10) {
-					n6 = chunk.getBlockId(px, py - 1, pz);
+				index = this.field_203 >> 2;
+				px = index & 15;
+				pz = index >> 8 & 15;
+				py = this.getHeightIterating(px | chunkX, pz | chunkZ);
+				if (this.getBiomeSource().getBiome(px | chunkX, pz | chunkZ).canSnow() && py >= 0 && py < 128 && chunk.getLight(LightType.BLOCK, px, py, pz) < 10) {
+					blockID = chunk.getBlockId(px, py - 1, pz);
 					int n9 = chunk.getBlockId(px, py, pz);
-					if (this.isRaining() && n9 == 0 && BaseBlock.SNOW.canPlaceAt(Level.class.cast(this), px + chunkX, py, pz + chunkZ) && n6 != 0 && n6 != BaseBlock.ICE.id && BaseBlock.BY_ID[n6].material.blocksMovement()) {
-						this.setBlock(px + chunkX, py, pz + chunkZ, BaseBlock.SNOW.id);
+					if (this.isRaining() && n9 == 0 && BaseBlock.SNOW.canPlaceAt(Level.class.cast(this), px | chunkX, py, pz | chunkZ) && blockID != 0 && blockID != BaseBlock.ICE.id && BaseBlock.BY_ID[blockID].material.blocksMovement()) {
+						this.setBlock(px | chunkX, py, pz | chunkZ, BaseBlock.SNOW.id);
 					}
-					if (n6 == BaseBlock.STILL_WATER.id && chunk.getMeta(px, py - 1, pz) == 0) {
-						this.setBlock(px + chunkX, py - 1, pz + chunkZ, BaseBlock.ICE.id);
+					if (blockID == BaseBlock.STILL_WATER.id && chunk.getMeta(px, py - 1, pz) == 0) {
+						this.setBlock(px | chunkX, py - 1, pz | chunkZ, BaseBlock.ICE.id);
 					}
 				}
 			}
 			
-			for (j = 0; j < 80; ++j) {
+			for (int k = 0; k < 80; ++k) {
 				this.field_203 = this.field_203 * 3 + 1013904223;
-				int index = this.field_203 >> 2;
-				px = index & 0xF;
-				pz = index >> 8 & 0xF;
-				py = index >> 16 & 0x7F;
-				int blockID = chunk.getBlockId(px, py, pz);
+				index = this.field_203 >> 2;
+				px = index & 15;
+				pz = index >> 8 & 15;
+				py = index >> 16 & 127;
+				blockID = chunk.getBlockId(px, py, pz);
 				if (!BaseBlock.TICKS_RANDOMLY[blockID]) continue;
-				BaseBlock.BY_ID[blockID].onScheduledTick(Level.class.cast(this), px + chunkX, py, pz + chunkZ, this.rand);
+				BaseBlock.BY_ID[blockID].onScheduledTick(Level.class.cast(this), px | chunkX, py, pz | chunkZ, this.rand);
 			}
 		}
 	}
